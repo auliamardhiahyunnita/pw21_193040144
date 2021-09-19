14033f2a080589507f2095323933bb589a08998d
auliamardhiahyunnita <19340144.aulia@mail.unpas.ac.id>
2021-09-19 20:49:37 +0700

    Menyelesaikan video pertemuan 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Aulia Mardhiah Yunnita
+193040144
+https://github.com/auliamardhiahyunnita/pw21_193040144.git
+pertemuan 2 - 19 September 2021
+Mempelajari mengenai sintaks PHP 
+*/   
+?>
+<?php
+// Standar Output
+//echo, print
+?>
